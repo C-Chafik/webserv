4150f064c33020479b7f04932638659dd89685dd
Hugo KOVAC <hkovac@e1r12p21.clusters.42paris.fr>
2022-10-08 20:27:47 +0200

    file location init root .

--- srcs/redirect.cpp
+++ srcs/redirect.cpp
@@ -2,6 +2,8 @@
 
 void Server::redirect(std::string &file, id_server_type server_id){
 
+	std::clog << "rtnFile : " << file << std::endl;
+
 	if (confs[server_id].locations.find(file) != confs[server_id].locations.end()
 		 && confs[server_id].locations[file].http_redirection.first == 301
 		 && confs[server_id].locations[file].http_redirection.second != file){
